498f3543309e44c85b7124a04879f2274e56f783
marko asplund <marko.asplund@ixonos.com>
2012-06-13 21:58:33 +0300

    KOKU-1287: comment regexp and stricter code checks

--- setup/git/msg-check.pl
+++ setup/git/msg-check.pl
@@ -1,15 +1,23 @@
 #!/usr/bin/perl
 
-open FILE, "<$ARGV[0]" or die $!;
+use strict;
+use warnings;
+
+#'<:encoding(utf8)'
+open FILE, "$ARGV[0]" or die $!;
 undef $/;
-$msg=<FILE>;
+my $msg=<FILE>;
 close(FILE);
 
 # automatic commit messages e.g. merge commits of the form:
 # Merge branch 'develop'
 # bypass service hooks so they don't need to be explicitly allowed.
 if ( $msg =~ /
-	(^KOKU-\d{1,5}:\ [\w\ ]{10,60}$(\n\n\w.*)?(^\#[^\r\n]*$)*)	# normal commit comment
+	(
+	^KOKU-\d{1,5}:\ [\w\ ]{10,60}$	# subject line (required)
+	(\n\n\w.*)?						# empty line + long description (optional)
+	(^\#[^\r\n]*$)*					# automatically included comments (optional)
+	)
 	/mx ) {
   exit(0);
 }
